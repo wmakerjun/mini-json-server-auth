f3ae1e5b2abc0afc04d5cb846bcb54f2b8c81f72
wonsss <bigdatajang@gmail.com>
2022-03-29 17:21:43 +0900

    feat: 회원가입 기능 구현

---
+++ src/signup.js
@@ -0,0 +1,37 @@
+const signup = event => {
+  event.preventDefault();
+  const name = document.querySelector("input[type='text']").value;
+  const email = document.querySelector("input[type='email']").value;
+  const password = document.querySelector("input[type='password']").value;
+
+  const url = "http://localhost:3000/signup/";
+  const data = {
+    email,
+    password,
+    name,
+  };
+
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(res => {
+      if (!res.ok) {
+        alert("회원가입 잘 쓰세요.>ㅇ<");
+        return;
+      }
+      return res.json();
+    })
+    .then(response => {
+      // console.log("성공", JSON.stringify(response));
+      const userAuth = JSON.stringify(response);
+      localStorage.setItem("userAuth", userAuth);
+      location.replace("../user.html");
+    })
+    .catch(error => console.error("에러", error));
+};
+
+document.querySelector("form").addEventListener("submit", signup);
